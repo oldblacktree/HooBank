a30b5e990b2b1b44b23b55794b65f1d13a381f39
Danil <oldblacktree@gmail.com>
2023-02-03 11:29:10 +0300

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+import {
+  people01,
+  people02,
+  people03,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  send,
+  shield,
+  star,
+} from "../assets";
