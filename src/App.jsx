48b6713e00bdb3a9729e20c4052adedefe7e9b4e
Danil <oldblacktree@gmail.com>
2023-02-04 20:59:19 +0300

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import {
+  Navbar
+} from './components';
 
 const App = () => {
-  return <div className="bg-primary"></div>;
+  return (
+    <div className="bg-primary min-h-[100vh]">
+      <div className="px-6 sm:px-16 flex justify-center items-center">
+        <div className="xl:max-w-[1280px] w-full"><Navbar /></div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import {logo, menu, close} from '../assets';
+
+const Navbar = () => {
+  const [isMenuOpen, toggleMenu] = useState(false)  
+
+  return (
+  <nav className='flex py-6 items-center w-full relative'>
+    <img src={logo} alt="hoobank logo" className='w-[124px] h-[32px]'/>
+    <ul className="list-none hidden sm:flex flex-1 justify-end font-poppins text-white text-base gap-10">
+      <li>
+        <a href="#home" className="block p-1 hover:opacity-80">Home</a>
+      </li>
+      <li>
+        <a href="#features" className="block p-1 hover:opacity-80">Features</a>
+      </li>
+      <li>
+        <a href="#product" className="block p-1 hover:opacity-80">Product</a>
+      </li>
+      <li>
+        <a href="#clients" className="block p-1 hover:opacity-80">Clients</a>
+      </li>
+    </ul>
+    <div className="flex flex-1 justify-end items-center sm:hidden">
+      <img src={isMenuOpen ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain'  onClick={() => toggleMenu((prev) => !prev)}/>
+      <div className={`${isMenuOpen ? 'flex' : 'hidden'} p-2 bg-black-gradient absolute top-20 right-0 min-w-[140px] rounded-xl sidebar`}>
+        <ul className="list-none flex flex-col flex-1 font-poppins text-white text-center text-lg">
+          <li>
+            <a href="#home" className="block p-5">Home</a>
+          </li>
+          <li>
+            <a href="#features" className="block p-5">Features</a>
+          </li>
+          <li>
+            <a href="#product" className="block p-5">Product</a>
+          </li>
+          <li>
+            <a href="#clients" className="block p-5">Clients</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
